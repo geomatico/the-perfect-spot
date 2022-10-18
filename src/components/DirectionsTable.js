1719fb2287c68bf2ed215b9e40c2e869a0d12cd9
Marti Pericay <marti.pericay@geomati.co>
2022-10-18 14:20:06 +0200

    medias

--- src/components/DirectionsTable.js
+++ src/components/DirectionsTable.js
@@ -13,8 +13,19 @@ const DirectionsTable = ({directions}) => {
 
   const locations = strOriginPoints ? JSON.parse(strOriginPoints) : [];
 
+  directions.forEach(function (element) {
+
+    let sum = 0;
+    for( var i = 0; i < element.data.length; i++ ){
+      sum += parseInt( element.data[i][1], 10 );
+
+    }
+    let avg = sum/element.data.length;
+    element.data.avg = Math.round( avg * 10)/10;
+  });
+
   // no nos mateis
-  const nombres = ['Mi casa', 'Gimnasio', 'Centro médico', 'Supermercado'];
+  const nombres = ['Mis padres', 'Mi trabajo', 'Amigo', 'Médico', 'Gimnasio'];
 
   return <>
     {
@@ -26,6 +37,7 @@ const DirectionsTable = ({directions}) => {
             {
               locations.map((location, i) => <TableCell key={location + i} align="right">{nombres[i]}</TableCell>)
             }
+            <TableCell key={'average'} align="right">Tiempo medio</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -46,6 +58,9 @@ const DirectionsTable = ({directions}) => {
                   }</TableCell>
                 ))
               }
+              <TableCell component="th" scope="row" align="center">
+                {row.data.avg} min
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
