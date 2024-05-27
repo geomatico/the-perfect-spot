3ebabba22905384b7a7ffb40e67906fbcd15a724
Joeljnq <118924347+joeljnq@users.noreply.github.com>
2024-05-27 10:50:14 +0200

    40-informative text has been updated by adding a blue point (#48)
    * 40-informative text has been updated by adding a blue point
    
    * Estilos dialog añadir punto
    
    * Cambios en traducción en inglés
    
    ---------
    
    Co-authored-by: martitasea <marta.pulido.asin@gmail.com>

--- src/components/ModalAddPoint.js
+++ src/components/ModalAddPoint.js
@@ -1,56 +1,53 @@
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import  PropTypes  from 'prop-types';
 import React from 'react';
-import Modal from '@mui/material/Modal';
+import PropTypes  from 'prop-types';
+
+import Typography from '@mui/material/Typography';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
 import TextField from '@mui/material/TextField';
-import {useTranslation} from 'react-i18next';
 import Button from '@mui/material/Button';
- 
+
+import {useTranslation} from 'react-i18next';
+import Box from '@mui/material/Box';
+
 const inputContainerStyles = {
   display: 'flex',
   flexDirection:'column',
-  alignItems:'center',
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  m: 4
+  justifyContent: 'space-between',
+  gap: 2
 };
-const  ModalAddPoint = ({pointType,pointName,onChangePointName,onSavePointName,onClose}) => {
+
+const  ModalAddPoint = ({pointType, pointName, onChangePointName, onSavePointName, onClose}) => {
   const {t} = useTranslation();
   const handleChangePoiName = pointName => onChangePointName(pointName);
   const handleSaveName = () => onSavePointName();
 
   const buttonColor = pointType === 'ADD_BLUE' ? 'primary' : 'secondary';
-
- 
-  return (
-    <Modal open={true} onClose={onClose}>
-      <Box sx={inputContainerStyles}>
-        <Typography id="modal-modal-title" variant="body1" sx={{textAlign:'center'}}> 
-          {pointType === 'ADD_POI' ? t('insertPOIName') : t('insertFLATName')}
+  
+  return <Dialog open={true} onClose={onClose} fullWidth>
+    <DialogContent sx={inputContainerStyles}>
+      <Box>
+        <Typography id="modal-modal-title" variant="body1">
+          {pointType === 'ADD_BLUE' ? t('insertBlueName') : t('insertRedName')}
         </Typography>
-        <TextField
-          error={!pointName}
-          helperText={!pointName ? t('mandatoryField') : ''}
-          sx={{mt: 2}}
-          value={pointName}
-          onChange={(e) => handleChangePoiName(e.target.value)}
-          variant="outlined"
-        />
-        <Box mt={2}>
-          <Button variant="contained" color={buttonColor} onClick={handleSaveName}>{t('done')}</Button>
-        </Box>
+        <Typography variant="body2" sx={{fontStyle: 'italic', color: 'text.secondary'}} gutterBottom>
+          {pointType === 'ADD_BLUE' ? t('insertBlueExample') : t('insertRedExample')}
+        </Typography>
       </Box>
-    </Modal>
-  );
+      
+      <TextField
+        error={!pointName}
+        helperText={!pointName ? t('mandatoryField') : ''}
+        size='small'
+        value={pointName}
+        onChange={(e) => handleChangePoiName(e.target.value)}
+        variant="outlined"
+      />
+      <Button variant="contained" color={buttonColor} onClick={handleSaveName} size='medium'>{t('done')}</Button>
+    </DialogContent>
+  </Dialog>;
 };
+
 ModalAddPoint.propTypes = {
   pointType: PropTypes.oneOf(['ADD_BLUE','ADD_RED']).isRequired,
   pointName: PropTypes.string.isRequired,
@@ -58,4 +55,5 @@ ModalAddPoint.propTypes = {
   onSavePointName: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired
 };
+
 export default ModalAddPoint;
